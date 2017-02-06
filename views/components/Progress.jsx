da509a9e66f63fd0c6551d0d8e740c29067767a0
cassidoo <cassidy@l4digital.com>
2017-02-06 14:09:35 -0800

    Add mock progress bar

--- views/components/Progress.jsx
+++ views/components/Progress.jsx
@@ -1,11 +1,31 @@
 'use babel';
 
 import React from 'react';
+import ProgressBar from 'react-progressbar.js';
 
 export default class Progress extends React.Component {
   render() {
+    var Bar = ProgressBar.Line;
+    var options1 = {
+      strokeWidth: 2,
+      color: '#62DCA5'
+    };
+    var options2 = {
+      strokeWidth: 2,
+      color: '#F7F879'
+    };
+
     return <div className="progress">
-      Progress!
+      <Bar
+        progress={0.5}
+        options={options2}
+        initialAnimate={true}
+        containerClassName={'pause-bar'} />
+      <Bar
+        progress={0.33}
+        options={options1}
+        initialAnimate={true}
+        containerClassName={'complete-bar'} />
     </div>;
   }
 }
