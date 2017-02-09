4950f97b1fd8f7c85027d6418174b3054406d4ba
cassidoo <cassidy@l4digital.com>
2017-02-09 11:33:43 -0800

    Add actual dates, remove mock dates

--- views/components/Date.jsx
+++ views/components/Date.jsx
@@ -1,18 +1,19 @@
 'use babel';
 
 import React from 'react';
+import Moment from 'moment';
 
 export default class Date extends React.Component {
   render() {
     return <div className="date">
       <div className="calendar">
-        <div className="day">24</div>
+        <div className="day">{Moment().date()}</div>
         <div className="my">
-          <div className="month">Jan</div>
-          <div className="year">2017</div>
+          <div className="month">{Moment().format('MMM')}</div>
+          <div className="year">{Moment().year()}</div>
         </div>
       </div>
-      <div className="today">Wednesday</div>
+      <div className="today">{Moment().format('dddd')}</div>
     </div>;
   }
 }
