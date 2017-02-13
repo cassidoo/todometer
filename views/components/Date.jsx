8cfe89b02bdfeedc0203cacdfe8908a93661df5c
cassidoo <cassidy@l4digital.com>
2017-02-12 21:33:52 -0800

    Move date info to state

--- views/components/Date.jsx
+++ views/components/Date.jsx
@@ -4,16 +4,46 @@ import React from 'react';
 import Moment from 'moment';
 
 export default class Date extends React.Component {
+  constructor() {
+    super();
+    this.state = { day: '',
+                   month: '',
+                   year: '',
+                   weekday: ''
+                 };
+  }
+
+  componentWillMount() {
+    this.setDate();
+  }
+
+  componentDidMount() {
+    window.setInterval(function () {
+      if (this.state.day !== Moment.date()) {
+        this.setDate();
+      }
+    }.bind(this), 1000);
+  }
+
+  setDate() {
+    this.setState({
+      day: Moment().date(),
+      month: Moment().format('MMM'),
+      year: Moment().year(),
+      weekday: Moment().format('dddd')
+    });
+  }
+
   render() {
     return <div className="date">
       <div className="calendar">
-        <div className="day">{Moment().date()}</div>
+        <div className="day">{this.state.day}</div>
         <div className="my">
-          <div className="month">{Moment().format('MMM')}</div>
-          <div className="year">{Moment().year()}</div>
+          <div className="month">{this.state.month}</div>
+          <div className="year">{this.state.year}</div>
         </div>
       </div>
-      <div className="today">{Moment().format('dddd')}</div>
+      <div className="today">{this.state.weekday}</div>
     </div>;
   }
 }
