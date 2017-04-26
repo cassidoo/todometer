af861de8c276b56babd2d978e2a1bc2950115ff5
cassidoo <cassidy@l4digital.com>
2017-04-26 02:35:07 -0700

    Make window persist on close

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 import { app, BrowserWindow } from 'electron';
 
 let mainWindow = null;
+let willQuit = false;
 
-app.on('window-all-closed', () => {
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
-});
+function createWindow() {
+  mainWindow = new BrowserWindow({width: 800, maxWidth: 800, height: 600, fullscreenable: false}); //, titleBarStyle: 'hidden'});
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+}
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({width: 800, maxWidth: 800, height: 600}); //, titleBarStyle: 'hidden'});
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-  // Open the DevTools.
-  //mainWindow.webContents.openDevTools();
+  createWindow();
+
+  mainWindow.on('close', (e) => {
+    if (willQuit) {
+      mainWindow = null;
+    } else {
+      e.preventDefault();
+      mainWindow.hide();
+    }
+  });
 });
+
+app.on('activate', () => mainWindow.show());
+app.on('before-quit', () => willQuitApp = true);
