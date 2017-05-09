7604fb70bdda22310a2b7b7dab035cf1dd938e0a
AymericM <aymeric.moehn@hotmail.fr>
2017-05-09 20:36:31 +0200

    Re indent
    2 spaces instead of 4 :)

--- main.js
+++ main.js
@@ -7,157 +7,159 @@ let willQuit = false;
 let tray = null;
 
 function createWindow() {
-    mainWindow = new BrowserWindow({
-        width: 800,
-        frame: false,
-        resizable: false,
-        height: 600,
-        fullscreenable: false,
-        backgroundColor: '#403F4D',
-        icon: path.join(__dirname, 'assets/png/128x128.png')
-    });
-    mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow = new BrowserWindow({
+    width: 800,
+    frame: false,
+    resizable: false,
+    height: 600,
+    fullscreenable: false,
+    backgroundColor: '#403F4D',
+    icon: path.join(__dirname, 'assets/png/128x128.png')
+  });
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
 }
 
 function manageRefresh() {
-    const time = moment('24:00:00', 'hh:mm:ss').diff(moment(), 'seconds');
-    setTimeout(midnightTask, time * 1000);
+  const time = moment('24:00:00', 'hh:mm:ss').diff(moment(), 'seconds');
+  setTimeout(midnightTask, time * 1000);
 
-    function midnightTask() {
-        mainWindow.reload();
-    }
+  function midnightTask() {
+    mainWindow.reload();
+  }
 }
 
 function traySetup() {
-    tray = new Tray(path.join(__dirname, 'assets/png/128x128.png'));
-    tray.on('click', () => {
-        mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show()
-    });
-    const contextMenu = Menu.buildFromTemplate([
-        {
-            label: 'Open todometer',
-            click: () => {
-                mainWindow.show();
-            }
-        }, {
-            label: 'About',
-            click: () => {
-                dialog.showMessageBox(mainWindow, {
-                    type: 'info',
-                    title: 'About',
-                    message: 'todometer is built by Cassidy Williams',
-                    detail: 'You can find her on GitHub and Twitter as @cassidoo, or on her website cassidoo.co.',
-                    icon: path.join(__dirname, 'assets/png/64x64.png')
-                });
-            }
-        }, {
-            type: 'separator'
-        }, {
-            label: 'Quit todometer',
-            click: () => {
-                app.quit();
-            }
-        }
-    ]);
-    tray.setToolTip('todometer');
-    tray.setContextMenu(contextMenu);
+  tray = new Tray(path.join(__dirname, 'assets/png/128x128.png'));
+  tray.on('click', () => {
+    mainWindow.isVisible()
+      ? mainWindow.hide()
+      : mainWindow.show()
+  });
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Open todometer',
+      click: () => {
+        mainWindow.show();
+      }
+    }, {
+      label: 'About',
+      click: () => {
+        dialog.showMessageBox(mainWindow, {
+          type: 'info',
+          title: 'About',
+          message: 'todometer is built by Cassidy Williams',
+          detail: 'You can find her on GitHub and Twitter as @cassidoo, or on her website cassidoo.co.',
+          icon: path.join(__dirname, 'assets/png/64x64.png')
+        });
+      }
+    }, {
+      type: 'separator'
+    }, {
+      label: 'Quit todometer',
+      click: () => {
+        app.quit();
+      }
+    }
+  ]);
+  tray.setToolTip('todometer');
+  tray.setContextMenu(contextMenu);
 }
 
 function menuSetup() {
-    const menuTemplate = [
+  const menuTemplate = [
+    {
+      label: 'todometer',
+      submenu: [
         {
-            label: 'todometer',
-            submenu: [
-                {
-                    label: 'About',
-                    click: () => {
-                        dialog.showMessageBox(mainWindow, {
-                            type: 'info',
-                            title: 'About',
-                            message: 'todometer is built by Cassidy Williams',
-                            detail: 'You can find her on GitHub and Twitter as @cassidoo, or on her website cassidoo.co.',
-                            icon: path.join(__dirname, 'assets/png/64x64.png')
-                        });
-                    }
-                }, {
-                    label: 'Contribute',
-                    click: () => {
-                        shell.openExternal('http://github.com/cassidoo/todometer');
-                    }
-                }, {
-                    type: 'separator'
-                    /* For debugging
+          label: 'About',
+          click: () => {
+            dialog.showMessageBox(mainWindow, {
+              type: 'info',
+              title: 'About',
+              message: 'todometer is built by Cassidy Williams',
+              detail: 'You can find her on GitHub and Twitter as @cassidoo, or on her website cassidoo.co.',
+              icon: path.join(__dirname, 'assets/png/64x64.png')
+            });
+          }
+        }, {
+          label: 'Contribute',
+          click: () => {
+            shell.openExternal('http://github.com/cassidoo/todometer');
+          }
+        }, {
+          type: 'separator'
+          /* For debugging
         }, {
           label: 'Dev tools',
           click: () => {
             mainWindow.webContents.openDevTools();
           }
           */
-                }, {
-                    label: 'Quit',
-                    accelerator: 'CommandOrControl+Q',
-                    click: () => {
-                        app.quit();
-                    }
-                }
-            ]
         }, {
-            label: 'Edit',
-            submenu: [
-                {
-                    role: 'undo'
-                }, {
-                    role: 'redo'
-                }, {
-                    role: 'cut'
-                }, {
-                    role: 'copy'
-                }, {
-                    role: 'paste'
-                }, {
-                    role: 'delete'
-                }, {
-                    role: 'selectall'
-                }
-            ]
-        }, {
-            label: 'View',
-            submenu: [
-                {
-                    role: 'reload'
-                }, {
-                    role: 'togglefullscreen'
-                }, {
-                    role: 'minimize'
-                }, {
-                    role: 'hide'
-                }, {
-                    role: 'close'
-                }
-            ]
+          label: 'Quit',
+          accelerator: 'CommandOrControl+Q',
+          click: () => {
+            app.quit();
+          }
         }
-    ];
-    const menu = Menu.buildFromTemplate(menuTemplate);
-    Menu.setApplicationMenu(menu);
+      ]
+    }, {
+      label: 'Edit',
+      submenu: [
+        {
+          role: 'undo'
+        }, {
+          role: 'redo'
+        }, {
+          role: 'cut'
+        }, {
+          role: 'copy'
+        }, {
+          role: 'paste'
+        }, {
+          role: 'delete'
+        }, {
+          role: 'selectall'
+        }
+      ]
+    }, {
+      label: 'View',
+      submenu: [
+        {
+          role: 'reload'
+        }, {
+          role: 'togglefullscreen'
+        }, {
+          role: 'minimize'
+        }, {
+          role: 'hide'
+        }, {
+          role: 'close'
+        }
+      ]
+    }
+  ];
+  const menu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(menu);
 }
 
 app.on('ready', () => {
-    createWindow();
-    if (process.platform === 'darwin') {
-        menuSetup();
+  createWindow();
+  if (process.platform === 'darwin') {
+    menuSetup();
+  }
+  traySetup();
+
+  mainWindow.on('close', (e) => {
+    if (willQuit) {
+      mainWindow = null;
+    } else {
+      e.preventDefault();
+      mainWindow.hide();
     }
-    traySetup();
+  });
 
-    mainWindow.on('close', (e) => {
-        if (willQuit) {
-            mainWindow = null;
-        } else {
-            e.preventDefault();
-            mainWindow.hide();
-        }
-    });
-
-    manageRefresh();
+  manageRefresh();
 });
 
 app.on('activate', () => mainWindow.show());
--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { createLogger } from 'redux-logger';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { loadState, saveState } from '../views/local-storage';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {createLogger} from 'redux-logger';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import {loadState, saveState} from '../views/local-storage';
 import rootReducer from '../views/reducers/index';
 import Main from '../views/main.jsx';
 
 const persistedState = loadState();
-let store = createStore(rootReducer,
-    persistedState,
-    composeWithDevTools(
-      applyMiddleware(
-        createLogger({ collapsed: true })
-      )
-    )
-  );
+let store = createStore(rootReducer, persistedState, composeWithDevTools(applyMiddleware(createLogger({collapsed: true}))));
 
 store.subscribe(() => {
   saveState(store.getState());
@@ -25,6 +18,6 @@ store.subscribe(() => {
 window.onload = function() {
   ReactDOM.render(
     <Provider store={store}>
-        <Main />
-    </Provider>, document.getElementById('app'));
+    <Main/>
+  </Provider>, document.getElementById('app'));
 };
