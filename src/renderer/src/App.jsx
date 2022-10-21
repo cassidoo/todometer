34a8775b985926f7e238b16510d3bd1c37f3d0a4
Nicky Meuleman <nicky.dev@outlook.com>
2022-10-21 23:06:51 +0200

    assets and public items

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from '../../../assets/react.svg'
 import './App.css'
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <img src="vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
