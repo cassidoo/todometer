9cf7df0cc45fa65a3287767692b125ac54946209
Nicky Meuleman <nicky.dev@outlook.com>
2022-10-21 22:44:48 +0200

    delete dist folders

--- src/preload/dist/index.cjs
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguY2pzIiwic291cmNlcyI6W10sInNvdXJjZXNDb250ZW50IjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiJ9
