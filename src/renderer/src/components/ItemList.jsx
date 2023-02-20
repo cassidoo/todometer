0f78846070b4213b5c001ae0f8a35109e83a800e
Cassidy Williams <1454517+cassidoo@users.noreply.github.com>
2023-02-19 23:50:47 -0600

    Moving more things to tabs instead of spaces

--- src/renderer/src/components/ItemList.jsx
+++ src/renderer/src/components/ItemList.jsx
@@ -1,8 +1,8 @@
 import {
-  Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionPanel,
+	Accordion,
+	AccordionItem,
+	AccordionButton,
+	AccordionPanel,
 } from "@reach/accordion";
 import "@reach/accordion/styles.css";
 
@@ -16,67 +16,67 @@ import alldone from "../img/alldone.svg";
 
 // List of todo items
 function ItemList() {
-  const dispatch = useAppReducer();
-  const { pending, paused, completed } = useItems();
+	const dispatch = useAppReducer();
+	const { pending, paused, completed } = useItems();
 
-  return (
-    <div className="item-list">
-      <Progress />
-      <AddItemForm />
-      {pending.length > 0 ? (
-        <>
-          {pending.map((item) => {
-            return <Item item={item} key={item.key} />;
-          })}
-        </>
-      ) : (
-        <div className={styles.alldone}>
-          <img src={alldone} alt="Nothing to do!" />
-        </div>
-      )}
-      <Accordion collapsible multiple>
-        {paused.length > 0 && (
-          <AccordionItem>
-            <AccordionButton className={styles.toggle}>
-              <img src={arrow} alt="Do Later Toggle" />
-              <span>Do Later</span>
-            </AccordionButton>
-            <AccordionPanel className={styles.panel}>
-              {paused &&
-                paused.map((item) => {
-                  return <Item item={item} key={item.key} />;
-                })}
-            </AccordionPanel>
-          </AccordionItem>
-        )}
-        {completed.length > 0 && (
-          <AccordionItem>
-            <AccordionButton className={styles.toggle}>
-              <img src={arrow} alt="Completed Toggle" /> <span>Completed</span>
-            </AccordionButton>
-            <AccordionPanel className={styles.panel}>
-              {completed &&
-                completed.map((item) => {
-                  return <Item item={item} key={item.key} />;
-                })}
-            </AccordionPanel>
-          </AccordionItem>
-        )}
-      </Accordion>
+	return (
+		<div className="item-list">
+			<Progress />
+			<AddItemForm />
+			{pending.length > 0 ? (
+				<>
+					{pending.map((item) => {
+						return <Item item={item} key={item.key} />;
+					})}
+				</>
+			) : (
+				<div className={styles.alldone}>
+					<img src={alldone} alt="Nothing to do!" />
+				</div>
+			)}
+			<Accordion collapsible multiple>
+				{paused.length > 0 && (
+					<AccordionItem>
+						<AccordionButton className={styles.toggle}>
+							<img src={arrow} alt="Do Later Toggle" />
+							<span>Do Later</span>
+						</AccordionButton>
+						<AccordionPanel className={styles.panel}>
+							{paused &&
+								paused.map((item) => {
+									return <Item item={item} key={item.key} />;
+								})}
+						</AccordionPanel>
+					</AccordionItem>
+				)}
+				{completed.length > 0 && (
+					<AccordionItem>
+						<AccordionButton className={styles.toggle}>
+							<img src={arrow} alt="Completed Toggle" /> <span>Completed</span>
+						</AccordionButton>
+						<AccordionPanel className={styles.panel}>
+							{completed &&
+								completed.map((item) => {
+									return <Item item={item} key={item.key} />;
+								})}
+						</AccordionPanel>
+					</AccordionItem>
+				)}
+			</Accordion>
 
-      {(completed.length > 0 || paused.length > 0) && (
-        <div className={styles.reset}>
-          <button
-            onClick={() => {
-              dispatch({ type: "RESET_ALL" });
-            }}
-          >
-            reset progress
-          </button>
-        </div>
-      )}
-    </div>
-  );
+			{(completed.length > 0 || paused.length > 0) && (
+				<div className={styles.reset}>
+					<button
+						onClick={() => {
+							dispatch({ type: "RESET_ALL" });
+						}}
+					>
+						reset progress
+					</button>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default ItemList;
