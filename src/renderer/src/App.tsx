e6deecf6ff8330b58f2e5235e9eefffdda8a9b10
liam-tait <liamr60@gmail.com>
2023-02-24 16:33:54 +1300

    convert App to ts

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,14 @@
+import TodoDate from "./components/TodoDate";
+import ItemList from "./components/ItemList";
+import { AppStateProvider } from "./AppContext";
+
+function App() {
+  return (
+    <AppStateProvider>
+      <TodoDate />
+      <ItemList />
+    </AppStateProvider>
+  );
+}
+
+export default App;
