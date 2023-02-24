72dfea3eb519b5ac66ba54ff676882f9fff158eb
liam-tait <liamr60@gmail.com>
2023-02-24 17:06:32 +1300

    convert App.test to ts

--- src/renderer/src/App.test.tsx
+++ src/renderer/src/App.test.tsx
@@ -1,7 +1,7 @@
 import { test, expect } from "vitest";
 import { render } from "@testing-library/react";
 import { format } from "date-fns";
-import App from "./App.jsx";
+import App from "./App.js";
 
 test("renders the day of the month", () => {
   const { getByText } = render(<App />);
